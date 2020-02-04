7ac8adecac430c99a61400b718754173be8611eb
Sanghoon Shin <shshin@shshin-imac.local>
2020-02-05 00:08:27 +0900

    second

--- renderer/items.js
+++ renderer/items.js
@@ -1,15 +1,98 @@
 let items = document.getElementById('items')
+const fs = require('fs')
+
+let readerJS
+fs.readFile(`${__dirname}/reader.js`, (err, data) => {
+  readerJS = data.toString()
+})
+
 exports.storage = JSON.parse(localStorage.getItem('readit-items')) || []
+
+window.addEventListener('message', e => {
+  if (e.data.action === 'delete-reader-item') {
+
+    this.delete(e.data.itemIndex)
+    e.source.close()
+  }
+})
+
+exports.getSelectedItem = () => {
+  let currenItem = document.getElementsByClassName('read-item selected')[0]
+
+  let itemIndex = 0
+  let child = currenItem
+  while((child = child.previousSibling) != null) itemIndex++
+
+  return { node: currenItem, index: itemIndex }
+}
+
+exports.delete = itemIndex => {
+  items.removeChild(items.childNodes[itemIndex])
+  this.storage.splice(itemIndex, 1)
+  this.save()
+
+  if (this.storage.length) {
+    let newSelectedItemIndex = (itemIndex === 0) ? 0 : itemIndex - 1
+
+    document.getElementsByClassName('read-item')[newSelectedItemIndex].classList.add('selected')
+  }
+}
 
 exports.save = () => {
   localStorage.setItem('readit-items', JSON.stringify(this.storage))
 }
 
+exports.select = e => {
+  this.getSelectedItem().node.classList.remove('selected')
+  e.currentTarget.classList.add('selected')
+}
+
+exports.changeSelection = direction => {
+  let currenItem = this.getSelectedItem()
+
+  if(direction === 'ArrowUp' && currenItem.node.previousSibling) {
+    currenItem.node.classList.remove('selected')
+    currenItem.node.previousSibling.classList.add('selected')
+  } else if (direction === 'ArrowDown' && currenItem.node.nextSibling ) {
+    currenItem.node.classList.remove('selected')
+    currenItem.node.nextSibling.classList.add('selected')
+  }
+
+}
+
+exports.open = () => {
+  if(!this.storage.length) return
+
+  let selectedItem = this.getSelectedItem()
+  let contentURL = selectedItem.node.dataset.url
+
+  let readerWin = window.open(contentURL, '', `
+    maxWidth=2000,
+    maxheight=2000,
+    width=1200,
+    height=800,
+    backgroundColor=#DEDEDE,
+    nodeIntegration=0,
+    contextIsolation=1
+  `)
+
+  readerWin.eval(readerJS.replace('{{index}}', selectedItem.index))
+
+}
+
 exports.addItem = (item, isNew = false) => {
   let itemNode = document.createElement('div')
   itemNode.setAttribute('class', 'read-item')
+  itemNode.setAttribute('data-url', item.url)
   itemNode.innerHTML = `<img src="${item.screenshot}"><h2>${item.title}</h2>`
   items.appendChild(itemNode)
+
+  itemNode.addEventListener('click', this.select)
+  itemNode.addEventListener('dblclick', this.open)
+
+  if(document.getElementsByClassName('read-item').length === 1) {
+    itemNode.classList.add('selected')
+  }
 
   if (isNew) {
     this.storage.push(item)
